e1ad87fead0d3e91036fc123d62ec4f08f8131d4
真心 <qgzhenxin@qq.com>
2023-03-28 11:00:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <el-config-provider :locale="locale"> </el-config-provider>
+</template>
+
+<script lang="ts" setup>
+import locale from "element-plus/es/locale/lang/zh-cn";
+</script>
